141ab25b953d6a07142f69a82ac6c3d33dcce576
Francisco Borges <franciscomsborges2002@gmail.com>
2026-02-02 00:08:44 +0000

    feat(steamTradeOffer): added first steps to have bot rep in trade offers

--- steamTradeOffer/content.js
+++ steamTradeOffer/content.js
@@ -12,9 +12,12 @@ const EXT_SCRIPT_INFO = {
 /**
 @description: Function to route the pathname to the scripts
 */
-function scriptRouter() {
+async function scriptRouter() {
   //Get the location of the website
   let url = new URL(window.location.href);
+
+  bots = await loadBotDb();
+  
   if (
     url.pathname.includes("tradeoffer") ||
     url.pathname.includes("tradeoffers")
@@ -24,6 +27,12 @@ function scriptRouter() {
     EXT_SCRIPT_INFO.scripts.push([
       "Partner Links",
       "https://github.com/Franciscoborges2002/tf2TradingUtils/tree/main/steamTradeOffer/partnerLinks",
+    ]);
+
+    loadBotRep(bots);
+    EXT_SCRIPT_INFO.scripts.push([
+      "Partner Links",
+      "https://github.com/Franciscoborges2002/tf2TradingUtils/tree/main/steamTradeOffer/",
     ]);
   }
   /* If there is the next in hostname, redirect to newUI scripts */
@@ -58,6 +67,34 @@ function loadPartnerLinks() {
 
     showPartnerLinks();
   })();
+}
+
+/* 
+Utility funtions to load scripts
+*/
+function loadBotRep(bots) {
+  (async () => {
+    // Load module dynamically
+    const { showBotRepTrade } = await import(
+      chrome.runtime.getURL("steamTradeOffer/botRep/content.js")
+    );
+
+    showBotRepTrade(bots);
+  })();
+}
+
+// Load bot DB JSON from extension package
+async function loadBotDb() {
+  try {
+    return await fetch(
+      "https://raw.githubusercontent.com/Franciscoborges2002/tf2TradingUtils/refs/heads/main/utils/data/trustedBots.json"
+    ).then((res) => {
+      if (!res.ok) throw new Error("Failed to fetch bots info");
+      return res.json();
+    });
+  } catch {
+    console.warn("[TF2TradingUtils]: Bot list failed to fetch");
+  }
 }
 
 /* 
